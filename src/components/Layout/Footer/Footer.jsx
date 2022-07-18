84533e1d9d104dfda2cabd479db854459dd97960
cuddleofdeath <cuddleofdeath@gmail.com>
2022-07-18 15:40:39 -0500

    added footer content

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 class Footer extends React.Component {
   render() {
-    return <footer>Footer</footer>;
+    return (
+      <footer>
+        Made with 💖 from{' '}
+        <a href='https://www.twitter.com/cuddleofdeath'>CuddleofDeath</a>
+      </footer>
+    );
   }
 }
 export default Footer;
